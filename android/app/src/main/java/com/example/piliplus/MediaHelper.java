4ce4212fd189da00e32b772ec277fa8f38aaaaeb
mashirozx <moezhx@outlook.com>
2026-07-30 13:22:13 +0800

    feat: ✨ mod for mashiro!

--- android/app/src/main/java/com/example/piliplus/MediaHelper.java
+++ android/app/src/main/java/com/example/piliplus/MediaHelper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.piliplus;
+package eu.mashiro.bilibili.pro;
 
 import android.app.PendingIntent;
 import android.content.ComponentName;
